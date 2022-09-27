157c3bfe0d1befb5b50c485e2b054b8293795bf1
oleg_nbd <oleg@nbd.by>
2022-09-27 11:28:01 +0300

    stages-work section accordions

--- src/js/app.js
+++ src/js/app.js
@@ -32,4 +32,34 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     });
     /* SWIPER */
+
+    /* ACCORDIONS */
+    const tabs = document.querySelectorAll('.stages-work__tab');
+    const tabsBtns = document.querySelectorAll('.stages-work-tab__btn');
+
+    const tabsRemoveActive = () => {
+        tabs.forEach(tab => tab.classList.remove('active'));
+    }
+
+    const tabAddActive = (index) => {
+        tabs[index].classList.add('active');
+
+        // if(index === 0) {
+        //     tabs[index + 1].firstElementChild.style.borderWidth = '2px 2px 2px 2px';
+        // }
+        // tabs[index - 1].firstElementChild.style.borderWidth = '2px 0px 2px 2px';
+        // if(index + 1 === tabs.length) {
+        //     tabs[index - 1].firstElementChild.style.borderWidth = '2px 2px 2px 2px';
+        // } else {
+        //     tabs[index - 1].firstElementChild.style.borderWidth = '2px 0px 2px 2px';
+        // }
+    }
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            tabsRemoveActive();
+            tabAddActive(index);
+        })
+    });
+    /* ACCORDIONS */
 });
