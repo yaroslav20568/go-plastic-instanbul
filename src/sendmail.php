c6f5c6203fe8adabed61cf03286f8969be4ec442
oleg_nbd <oleg@nbd.by>
2022-11-09 14:29:47 +0300

    add phpmailer

--- src/sendmail.php
+++ src/sendmail.php
@@ -1,23 +1,44 @@
 <?php
-	$to = "ravlushevich.iarek@mail.ru";
-	$tema = "Связаться с нами";
-	foreach($_POST as $key => $value){
-		$message = "Ваше имя: ".$_POST[$key]."<br>";
+$rdir = str_replace("\\", "/", __DIR__);
+require $rdir.'/phpmailer/Exception.php';
+require $rdir.'/phpmailer/PHPMailer.php';
+require $rdir.'/phpmailer/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+$mail = new PHPMailer(true);
+
+$mail->From = "noreply@dev.nbd.by";
+$mail->FromName = $_POST['name'];
+
+
+$mail->addAddress("yaroslav@nbd.by", "Recepient Name");
+$mail->addAddress("yaroslav@nbd.by");
+
+
+
+$mail->isHTML(true);
+
+foreach ($_POST as $key => $value)
+	if($key == 'name') {
+		$message = "<b>Ваше имя:</b> ".$value."<br>";
+	} else if($key == 'phone') {
+		$message .= "<b>Телефон:</b> ".$value."<br>";
+	} else if($key == 'email') {
+		$message .= "<b>E-mail:</b> ".$value."<br>";
+	} else if($key == 'message') {
+		$message .= "<b>Сообщение:</b> ".$value."<br>";
 	}
-	foreach ($_POST as $key => $value)
-		if($key == 'name') {
-			$message = "Ваше имя: ".$value."<br>";
-		} else if($key == 'phone') {
-			$message .= "Телефон: ".$value."<br>";
-		} else if($key == 'email') {
-			$message .= "E-mail: ".$value."<br>";
-		} else if($key == 'message') {
-			$message .= "Сообщение: ".$value."<br>";
-		}
-	// $message = "Ваше имя: ".$_POST['name']."<br>";
-	// $message .= "E-mail: ".$_POST['email']."<br>";
-	// $message .= "Сообщение: ".$_POST['message']."<br>";
-	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-	mail($to, $tema, $message, $headers);
-?>
+
+$mail->Subject = "Связаться с нами";
+$mail->Body = $message;
+
+try {
+    $mail->send();
+    echo "Message has been sent successfully";
+} catch (Exception $e) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
